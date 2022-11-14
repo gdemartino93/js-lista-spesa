caaffee201741d430c64ac966333ad91476950c0
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-14 14:11:01 +0100

    bonus. todoolist adapted with js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,35 @@
 const listaspesa=["pane", "salsa", "frutta","asd" ]
-let container=document.getElementById("container");
+let container=document.querySelector(".nomefeature");
+
 // for (let i = 0; i< listaspesa.length; i++){
 //     container.innerHTML+=`<span>${listaspesa[i]}</span>`
-//     console.log("L'elemento " + i + " è: " + listaspesa[i])
 // }
-
 let addBtn=document.getElementById("add");
+
 addBtn.addEventListener("click",
 function(){
 let nuovo=prompt("inserisci nuovo item della spesa")
-listaspesa.push("nuovo")
-container.innerHTML+=`<span>${nuovo}</span>`
-})
+listaspesa.push(nuovo)
+container.innerHTML+=`
+<div class="nomefeature">
+<input type="checkbox" name="" id="">
+<span class="taskname"> ${nuovo}</span>
+<span class="label greenyellow">Added</span>
 
+    </div> 
+`
+})
 i=0
 while (i < listaspesa.length) {
-    container.innerHTML+=`<span>${listaspesa[i]}</span>`
+    container.innerHTML+=
+    `
+    <div class="nomefeature">
+    <input type="checkbox" name="" id="">
+    <span class="taskname"> ${listaspesa[i]}</span>
+    <span class="label blue">Dalla lista</span>
+        </div> 
+    `
     i++;
   }
+
+  
